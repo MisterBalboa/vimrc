dc0180edbfe62d99d4b68b77b766937474adcb97
MisterBalboa <ignacio.palma@live.com>
2021-01-01 21:21:15 -0500

    source bash

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 if [ $(uname -s) = "Darwin" ]
 then
   echo "Installing vimplug for MacOS..."
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  curl -fLos ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 if [ $(uname -s) = "Linux" ]
 then
   echo "Installing vimplug for Linux..."
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  sh -s -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
@@ -19,3 +19,13 @@ cd ../ && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 cd vimrc && cat vimrc > ~/.vimrc
+
+if [ -f ~/.bashrc ]
+then
+  source ~/.bashrc
+fi
+
+if [ -f ~/.zshrc ]
+then
+  source ~/.zshrc
+fi
